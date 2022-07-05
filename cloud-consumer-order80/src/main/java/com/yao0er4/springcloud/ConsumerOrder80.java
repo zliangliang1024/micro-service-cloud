918e68b22c2c1b6bca0bd892269c2c5de8dde5d3
yao0er4 <zliangliang1024@126.com>
2022-07-05 23:52:18 +0800

    stand-alone eureka

--- cloud-consumer-order80/src/main/java/com/yao0er4/springcloud/ConsumerOrder80.java
+++ cloud-consumer-order80/src/main/java/com/yao0er4/springcloud/ConsumerOrder80.java
@@ -2,8 +2,10 @@ package com.yao0er4.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerOrder80 {
 
     public static void main(String[] args) {
